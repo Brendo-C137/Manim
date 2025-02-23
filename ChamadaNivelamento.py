c965265e48592b7d15c9a4a4db913aba26542181
Brendo-C137 <brendo.carvalho@itec.ufpa.br>
2025-02-23 20:48:17 -0300

    removing comments from ChamadoNivelamento

--- ChamadaNivelamento.py
+++ ChamadaNivelamento.py
@@ -12,14 +12,10 @@ class ChamadaNivelamento(Scene):
 
         self.wait(0.2)
 
-# class Texto(Scene):
-#     def construct(self):
         txt = Text("Tem Dificuldade com").to_edge(UP, buff=1)
         txt2 = Text("Matemática?",color="#2357A0").next_to(txt, DOWN)
         equation = MathTex("f(x)=2.\sin(3x\pi)")
 
-# class CreateGraph(Scene):
-#     def construct(self):
         axes = Axes(x_range=[-3, 3], y_range=[-5, 5], axis_config={"color": BLUE}).scale(0.5).next_to(equation, DOWN)
         graph = axes.plot(lambda x: 2*np.sin(3*x*np.pi), color=RED)
         graph_label = axes.get_graph_label(graph, color=BLUE)
@@ -29,10 +25,9 @@ class ChamadaNivelamento(Scene):
         self.play(Write(equation))
         self.play(Create(axes), Create(graph))
         self.wait(1)
+
         self.play(FadeOut(txt, txt2, equation, axes, graph))
 
-# class Encerramento(Scene):
-#     def construct(self):
         svg_pc = SVGMobject("D:\DF\VSCodium\SVGs\SVG\LogoPC.svg").scale(2)
         txt_end = Text("Participe do eixo de").next_to(svg_pc, UP)
         txt_end2 = Text("Pré-Cálculo").set_color_by_gradient("#2357A0").next_to(svg_pc, DOWN)
@@ -40,4 +35,5 @@ class ChamadaNivelamento(Scene):
         self.play(Write(txt_end))
         self.play(Write(txt_end2, run_time=1), FadeIn(svg_pc))
         self.wait(1)
+        
         self.play(FadeOut(txt_end), FadeOut(txt_end2), FadeOut(svg_pc))
